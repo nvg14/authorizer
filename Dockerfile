06da789002bf74ca234f8765f7c3da0dd903c1f5
Nithin V Gopal <nithinvg1495@gmail.com>
2023-04-13 16:13:20 +0530

    test

--- Dockerfile
+++ Dockerfile
@@ -1,32 +1,57 @@
-FROM golang:alpine AS builder
+# FROM golang:alpine AS builder
 
-RUN apk update && apk add --no-cache git tzdata
-RUN apk add ca-certificates && update-ca-certificates
+# RUN apk update && apk add --no-cache git tzdata
+# RUN apk add ca-certificates && update-ca-certificates
 
-WORKDIR $GOPATH/src/authorizer/
+# WORKDIR $GOPATH/src/authorizer/
 
-COPY go.mod .
+# COPY go.mod .
 
-RUN go mod download
-RUN go mod verify
+# RUN go mod download
+# RUN go mod verify
 
-COPY . .
+# COPY . .
 
-RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w' -o /go/bin/authorizer cmd/authorizer/*.go
+# RUN CGO_ENABLED=0 GOOS=linux GOARCH=amd64 go build -a -tags netgo -ldflags '-w' -o /go/bin/authorizer cmd/authorizer/*.go
 
+
+# FROM ubuntu:latest
+
+# COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
+# COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
+# COPY --from=builder /go/bin/authorizer /go/bin/authorizer
+
+# WORKDIR /go/bin/
+
+# COPY ./entrypoint.sh .
+# COPY ./scope_to_rules.json .
+
+# RUN apt-get update
+# RUN apt-get install -y jq
+
+# RUN chmod +x entrypoint.sh
 
 FROM ubuntu:latest
 
-COPY --from=builder /usr/share/zoneinfo /usr/share/zoneinfo
-COPY --from=builder /etc/ssl/certs/ca-certificates.crt /etc/ssl/certs/
-COPY --from=builder /go/bin/authorizer /go/bin/authorizer
+RUN apt-get update && apt-get install -y \
+        software-properties-common
+RUN add-apt-repository ppa:deadsnakes/ppa
+RUN apt-get update && apt-get install -y \
+    python3.7 \
+    python3-pip
+RUN python3.7 -m pip install pip
+RUN apt-get update && apt-get install -y \
+    python3-distutils \
+    python3-setuptools
+RUN python3.7 -m pip install pip --upgrade pip
 
-WORKDIR /go/bin/
+RUN pip3 install jwt
+RUN pip3 install re
+RUN pip3 install jsonpath_ng
+RUN pip3 install urllib3
 
-COPY ./entrypoint.sh .
-COPY ./scope_to_rules.json .
+COPY . /app/
 
-RUN apt-get update
-RUN apt-get install -y jq
+WORKDIR /app
 
-RUN chmod +x entrypoint.sh
+CMD [ "python3", "./test.py"]
---
+++ test.py
@@ -0,0 +1,208 @@
+import http.server
+import socketserver
+import json
+import sys
+from urllib.parse import urlparse
+
+sys.path.append("/opt/homebrew/lib/python3.10/site-packages")
+import jwt
+import re
+import jsonpath_ng as jp
+
+authorization_rules = {}
+with open('scope_to_rules_v2.json') as json_file:
+    data = json.load(json_file)
+    for item in data:
+        authorization_rules[item["id"]] = item
+        operations = list(item["operations"].keys())
+        for operation in operations:
+            api_path_patterns = list(item["operations"][operation].keys())
+            for path_pattern in api_path_patterns:
+                scope_data = item["operations"][operation][path_pattern]
+                temp = "/" + operation + path_pattern
+                temp = temp.replace("/", "\/")
+                resources = list(item["operations"][operation][path_pattern]["resources"].keys())
+                for resource in resources:
+                    temp = temp.replace(":" + resource, str(item["operations"][operation][path_pattern]["resources"][resource]))
+                authorization_rules[item["id"]]["operations"][operation][temp] = scope_data
+                authorization_rules[item["id"]]["operations"][operation].pop(path_pattern)
+
+
+PORT = 8000
+Handler = http.server.SimpleHTTPRequestHandler
+PREFIX = 'Bearer '
+
+def get_token(header):
+    if not header.startswith(PREFIX):
+        raise ValueError('Invalid token')
+
+    return header[len(PREFIX):]
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import logging
+
+class S(BaseHTTPRequestHandler):
+    def _set_allowed_response(self):
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+    
+    def _set_denied_response(self):
+        self.send_response(403)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+    def check_scope(self, permission):
+        
+        rules = authorization_rules[permission]
+        path = self.path
+        url = urlparse(path)
+        print(url)
+        operation = url.path.split("/")[1]
+        # operation = self.headers.get("x-operation-id")
+        if operation == None:
+            return False
+
+        if operation not in rules["operations"]:
+            return False
+        
+        operation_scope = rules["operations"][operation]
+        
+        
+        
+        for pattern, scope in operation_scope.items():
+            temp = re.compile(pattern)
+            
+            if temp.match(url.path) != None:
+                if self.command not in scope["actions"]:
+                    return False
+
+                if "conditions" not in scope:
+                    return True
+                
+                print("Checking Headers")
+                if "headers" in scope["conditions"]:
+                    for header, value in scope["conditions"]["headers"].items():
+                        match_count = 0
+                        request_header_values = self.headers.get(header)
+                        if request_header_values == None:
+                            return False
+                        
+                        # request_header_values = request_header_values.split(",")
+                        for item in value:
+                            if item == request_header_values:
+                                match_count += 1
+                        
+                        if match_count == 0:
+                            return False
+                        # if len(set(request_header_values) - set(value)) != 0:
+                        #     return False
+                
+                print("Checking Query")
+                if "query" in scope["conditions"]:
+                    for query, values in scope["conditions"]["query"].items():
+                        match_count = 0
+                        query_params = []
+                        for value in values:
+                            query_params.append(query + "=" + value)
+                        
+                        request_queries = url.query.split("&")
+                        for q in query_params:
+                            if q in request_queries:
+                                match_count += 1
+                        
+                        if match_count == 0:
+                            return False
+                
+                print("Checking Body")
+                if "body" in scope["conditions"]:
+                    for key, values in scope["conditions"]["body"].items():
+                        content_length = int(self.headers.get("Content-Length"))
+                        body = self.rfile.read(content_length)
+                        body = self.convert_bytes_to_json(body)
+                        query = jp.parse(key)
+                        query_result = query.find(body)
+                        
+                        if len(query_result) == 0:
+                            return False
+                        
+                        query_value = query_result[0].value
+
+                        if query_value not in values:
+                            return False
+                        
+                return True
+
+            else:
+                return False
+        # if self.command not in 
+
+    def convert_bytes_to_json(self, body):
+        my_json = body.decode('utf8').replace("'", '"')
+        # Load the JSON to a Python list & dump it back out as formatted JSON
+        return json.loads(my_json)
+        # return json.dumps(data, indent=4, sort_keys=True)
+
+    def do_GET(self):
+        logging.info("GET request,\nPath: %s\nHeaders:\n%s\n", str(self.path), str(self.headers))
+        authz = self.headers.get("Authorization")
+        
+        if authz == None:
+            
+            self._set_denied_response()
+            return
+        
+        decoded_data = jwt.decode(get_token(authz), "RS256", algorithms=["HS256"], options={"verify_signature": False})
+        if "permissions" not in decoded_data:
+            
+            self._set_denied_response()
+            return
+        
+        permissions = decoded_data["permissions"]
+        for permission in permissions:
+            
+            if permission not in authorization_rules:
+                
+                continue
+            if not self.check_scope(permission):
+                
+                self._set_denied_response()
+                return
+            else:
+                
+                self._set_allowed_response()
+                return
+        
+        self._set_denied_response()
+        return
+
+
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length']) # <--- Gets the size of data
+        post_data = self.rfile.read(content_length) # <--- Gets the data itself
+        logging.info("POST request,\nPath: %s\nHeaders:\n%s\n\nBody:\n%s\n",
+                str(self.path), str(self.headers), post_data.decode('utf-8'))
+
+        self._set_response()
+        self.wfile.write("POST request for {}".format(self.path).encode('utf-8'))
+
+def run(server_class=HTTPServer, handler_class=S, port=8080):
+    logging.basicConfig(level=logging.INFO)
+    server_address = ('', port)
+    httpd = server_class(server_address, handler_class)
+    logging.info('Starting httpd...\n')
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    logging.info('Stopping httpd...\n')
+
+if __name__ == '__main__':
+    from sys import argv
+
+    if len(argv) == 2:
+        run(port=int(argv[1]))
+    else:
+        run()
